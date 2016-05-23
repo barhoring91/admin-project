40b10fdd8f90e02047e423343ed491442308e222
Bar Horing <bar.apple@gmail.com>
2016-05-23 10:46:52 +0300

    better preformance

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,28 +1,24 @@
 class AccountsController < ActionController::Base
   #id is app_key
   def get_account_info
-    temp = ActivityType.find_by(name: 'search account info')
-    activity_id = (temp.attributes)['id']
+    activity_id = ActivityType.where(name: 'search account info').pluck('id')[0]
     Log.create(app_key: params[:app_key], admin_user_id: 12, activity_type_id: activity_id)
     render :json => ApiRequest::Account.get_account_info(params[:app_key])
   end
 
   def get_account_features
-    temp = ActivityType.find_by(name: 'search account features')
-    activity_id = (temp.attributes)['id']
+    activity_id = ActivityType.where(name: 'search account features').pluck('id')[0]
     Log.create(app_key: params[:app_key], admin_user_id: 12, activity_type_id: activity_id)
     render :json => ApiRequest::Account.get_features_for_account(params[:app_key])
   end
 
   def enable_feature
-    temp = ActivityType.find_by(name: 'enable_feature')
-    activity_id = (temp.attributes)['id']
+    activity_id = ActivityType.where(name: 'enable_feature').pluck('id')[0]
     Log.create(app_key: params[:app_key], admin_user_id: 12, activity_type_id: activity_id)
     render :json => ApiRequest::Account.enable_feature(params[:app_key], params[:feature_id])
   end
   def disable_feature
-    temp = ActivityType.find_by(name: 'disable_feature')
-    activity_id = (temp.attributes)['id']
+    activity_id = ActivityType.where(name: 'disable_feature').pluck('id')[0]
     Log.create(app_key: params[:app_key], admin_user_id: 12, activity_type_id: activity_id)
     render :json => ApiRequest::Account.disable_feature(params[:app_key], params[:feature_id])
   end
--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,7 +1,6 @@
 class FeaturesController < ActionController::Base
   def get_all_features
-    temp = ActivityType.find_by(name: 'search all features')
-    activity_id = (temp.attributes)['id']
+    activity_id = ActivityType.where(name: 'search all features').pluck('id')[0]
     Log.create(app_key: 'dummy', admin_user_id: 12, activity_type_id: activity_id)
     render :json => ApiRequest::Feature.get_all_features
   end
