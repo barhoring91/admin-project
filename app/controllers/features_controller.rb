dd701a78d36c5ddd1a0ef310390076fe444ec4fb
Bar Horing <bar.apple@gmail.com>
2016-05-23 10:25:57 +0300

    added search all log

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,5 +1,8 @@
 class FeaturesController < ActionController::Base
   def get_all_features
+    temp = ActivityType.find_by(name: 'search all features')
+    activity_id = (temp.attributes)['id']
+    Log.create(app_key: 'dummy', admin_user_id: 12, activity_type_id: activity_id)
     render :json => ApiRequest::Feature.get_all_features
   end
 end
