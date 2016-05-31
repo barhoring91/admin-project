71ba2acb8e4d81b5cdc936efc28fe329b2d40ece
Bar Horing <bar.apple@gmail.com>
2016-05-31 14:08:57 +0300

    login start

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,22 @@
 class LoginController < ApplicationController
   def signin
+    if(!(params[:email]))
+      render 'signin'
+    else
+    puts check_valid_user(params[:email], params[:password]);
+    end
+  end
+  def check_valid_user(email, password)
+    user = AdminUser.find_by_email(email)
+    a = valid_password(user, password)
+  end
+  def valid_password(user, password)
+    u_password = user.attributes['password']
+    if u_password == password
+      #create_token();
+    else
+      @valid = false
+      render 'signin'
+    end
   end
 end
